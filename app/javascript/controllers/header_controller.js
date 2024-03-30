ff8f36231c25be046285758b58c8d79626d6cd11
derikbf <derikb@gmail.com>
2024-03-30 09:26:44 -0300

    feat: add el-transition login

---
+++ app/javascript/controllers/header_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+import { enter, leave, toggle } from 'el-transition'
+
+export default class extends Controller {
+
+  static targets = [ 'dropdown', 'openUserMenu' ]
+
+  connect () {
+    this.openUserMenuTarget.addEventListener("click", (e) => {
+      openDropdown(this.dropdownTarget)
+    })
+  }
+}
+
+function openDropdown(element) {
+  toggle(element).then(() => {
+      console.log("Enter transition complete")
+  })
+}
+
+// remove element when close
+function closeDropdown(element) {
+  leave(dropdownTarget).then(() => {
+      element.destroy();
+  })
+}
