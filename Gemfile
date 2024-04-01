1f60dd6ea22c0958754d059daf61ad0981922a4d
derikbf <derikb@gmail.com>
2024-04-01 07:32:39 -0300

    feat: add gem faker

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem "rails", "~> 7.1.3", ">= 7.1.3.2"
 
 gem 'devise', '~> 4.9', '>= 4.9.3'
 
+gem 'faker'
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
@@ -65,3 +67,4 @@ group :development do
 end
 
 gem "tailwindcss-rails", "~> 2.3"
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,24 +8,15 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-Property.create!({
-  name: 'Sample Property',
-  description: 'Sample Property description',
-  headline: 'Sample Property headline',
-  address_1: 'Sample Property address_1',
-  address_2: 'Sample Property address_2',
-  city: 'City 1',
-  state: 'State 1',
-  country: 'Country 1'
-})
-
-Property.create!({
-  name: 'Sample Property 2',
-  description: 'Sample Property description 2',
-  headline: 'Sample Property headline 2',
-  address_1: 'Sample Property address 2',
-  address_2: 'Sample Property address 2',
-  city: 'City 2',
-  state: 'State 2',
-  country: 'Country 2'
-})
+20.times do 
+  Property.create!({
+    name: Faker::Lorem.unique.sentence(word_count: 3),
+    description: Faker::Lorem.paragraph(sentence_count: 10),
+    headline: Faker::Lorem.unique.sentence(word_count: 6),
+    address_1: Faker::Address.street_address,
+    address_2: Faker::Address.street_name,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    country: Faker::Address.country
+  })
+end
