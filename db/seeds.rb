bd547825a896b5561872e7b91ec295d3e8c9c318
derikbf <derikb@gmail.com>
2024-04-01 12:43:16 -0300

    feat: add reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,11 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+user = User.create!({
+  email: 'test1@gmail.com',
+  password: '123456'
+})
+
 6.times do |i|
   property = Property.create!({
     name: Faker::Lorem.unique.sentence(word_count: 3),
@@ -23,4 +28,18 @@
 
   property.images.attach(io: File.open("db/images/property_#{i + 1}.png"), filename: property.name )
   property.images.attach(io: File.open("db/images/property_#{i + 7}.png"), filename: property.name )
+
+  ((50..100).to_a.sample).times do 
+    Review.create!({
+      content: Faker::Lorem.paragraph(sentence_count: 10),
+      cleanliness_rating: (1..5).to_a.sample,
+      accuracy_rating: (1..5).to_a.sample,
+      checkin_rating: (1..5).to_a.sample,
+      communication_rating: (1..5).to_a.sample,
+      location_rating: (1..5).to_a.sample,
+      value_rating: (1..5).to_a.sample,
+      property: property,
+      user: user
+    })
+  end
 end
