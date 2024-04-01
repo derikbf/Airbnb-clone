e742513f99b4a01d339bfe99e43641f1ef2e63bb
derikbf <derikb@gmail.com>
2024-04-01 07:21:31 -0300

    feat: creating property model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Property.create!({
+  name: 'Sample Property',
+  description: 'Sample Property description',
+  headline: 'Sample Property headline',
+  address_1: 'Sample Property address_1',
+  address_2: 'Sample Property address_2',
+  city: 'City 1',
+  state: 'State 1',
+  country: 'Country 1'
+})
+
+Property.create!({
+  name: 'Sample Property 2',
+  description: 'Sample Property description 2',
+  headline: 'Sample Property headline 2',
+  address_1: 'Sample Property address 2',
+  address_2: 'Sample Property address 2',
+  city: 'City 2',
+  state: 'State 2',
+  country: 'Country 2'
+})
