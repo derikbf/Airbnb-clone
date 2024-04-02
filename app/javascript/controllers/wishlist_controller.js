7a4e653c11fdaa7039f6538cfc6c9b8effea42ab
derikbf <derikb@gmail.com>
2024-04-02 13:08:20 -0300

    fix minor error wishlist controller js

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -3,15 +3,21 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   updateWishlistStatus() {
+    const isUserLoggedIn = this.element.dataset.userloggedIn;
+    if ( isUserLoggedIn === "false"){
+      document.querySelector(".js-login").click();
+      return
+    }
+
     if (this.element.dataset.status == "false") {
       this.element.classList.remove("fill-none");
       this.element.classList.add("fill-primary");
-      this.element.dataset.status == "true";
+      this.element.dataset.status = "true";
     } 
     else {
       this.element.classList.remove("fill-primary");
       this.element.classList.add("fill-none");
-      this.element.dataset.status == "false";
+      this.element.dataset.status = "false";
     }
   }
 }
